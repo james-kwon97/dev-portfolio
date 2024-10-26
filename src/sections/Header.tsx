72c6508eeacbae6e355bb16583b2b9f2fab27acc
James Kwon <jhk1097@gmail.com>
2024-10-26 19:44:49 +1300

    Add initial styling to navbar with Tailwind

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,8 +1,13 @@
 export const Header = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center relative top-3">
       <nav>
-        <a href="#">Home</a>
+        <a
+          href="#"
+          className="px-4 py-1.5 rounded-full text-white/70 text-sm font-semibold"
+        >
+          Home
+        </a>
         <a href="#">Projects</a>
         <a href="#">About</a>
         <a href="#">Contact</a>
