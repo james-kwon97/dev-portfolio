0f8f80360b39a5aeabd51d78b9a8893eddff3b78
James Kwon <jhk1097@gmail.com>
2024-10-28 23:21:03 +1300

    Styling my hero section using tailwind

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,18 +4,20 @@ import ArrowDown from '@/assets/icons/arrow-down.svg';
 
 export const HeroSection = () => {
   return (
-    <div>
+    <div className="py-32 ">
       <div className="container">
-        <Image src={memojiImage} alt="Person behind laptop emoji" />
-        <div>
-          <div></div>
-          <div>Available for new projects</div>
+        <div className="flex flex-col items-center ">
+          <Image src={memojiImage} className="size-[100px]" alt="Person behind laptop emoji" />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-semibold ">Available for new projects</div>
+          </div>
+          <h1>Building Exceptional User Experiences</h1>
+          <p>
+            I specialise in transformation designs into functional, high-performing web
+            applications. Let's discuss your next project.
+          </p>
         </div>
-        <h1>Building Exceptional User Experiences</h1>
-        <p>
-          I specialise in transformation designs into functional, high-performing web applications.
-          Let's discuss your next project.
-        </p>
       </div>
       <button>
         <span>Explore My Work</span>
