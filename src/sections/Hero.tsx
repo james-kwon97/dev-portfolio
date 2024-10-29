6d84fc8cdb0d89eaa47382bbbaf4a53b33cbd56a
James Kwon <jhk1097@gmail.com>
2024-10-29 21:43:27 +1300

    Completed styling Hero section using tailwind css

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,23 +10,27 @@ export const HeroSection = () => {
           <Image src={memojiImage} className="size-[100px]" alt="Person behind laptop emoji" />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
             <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-semibold ">Available for new projects</div>
+            <div className="text-sm font-medium ">Available for new projects</div>
           </div>
-          <h1>Building Exceptional User Experiences</h1>
-          <p>
+          <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60">
             I specialise in transformation designs into functional, high-performing web
             applications. Let's discuss your next project.
           </p>
+          <div className="flex flex-col items-center mt-8 gap-4">
+            <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
+              <span className='font-semibold'>Explore My Work</span>
+              <ArrowDown className='size-4/>
+            </button>
+            <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl>
+              <span>👋🏻</span>
+              <span className='font-semibold '>Let's Connect!</span>
+            </button>
+          </div>
         </div>
       </div>
-      <button>
-        <span>Explore My Work</span>
-        <ArrowDown />
-      </button>
-      <button>
-        <span>👋🏻</span>
-        <span>Let's Connect!</span>
-      </button>
     </div>
   );
 };
