93d243cbc9f8c2f512eaed0f8b254adfda18e4b9
James Kwon <jhk1097@gmail.com>
2024-10-29 21:45:11 +1300

    Fixed missing quotation errors

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -20,13 +20,13 @@ export const HeroSection = () => {
             applications. Let's discuss your next project.
           </p>
           <div className="flex flex-col items-center mt-8 gap-4">
-            <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
-              <span className='font-semibold'>Explore My Work</span>
-              <ArrowDown className='size-4/>
+            <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+              <span className="font-semibold">Explore My Work</span>
+              <ArrowDown className="size-4" />
             </button>
-            <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl>
+            <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
               <span>👋🏻</span>
-              <span className='font-semibold '>Let's Connect!</span>
+              <span className="font-semibold ">Let's Connect!</span>
             </button>
           </div>
         </div>
