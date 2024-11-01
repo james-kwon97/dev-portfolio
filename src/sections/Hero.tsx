b8753a4ed4ab5269bf4de79a00e4de008c41dd26
James Kwon <jhk1097@gmail.com>
2024-11-01 23:44:05 +1300

    Adding sparkle icons into the hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 import grainImage from '@/assets/images/grain.jpg';
 import StarIcon from '@/assets/icons/star.svg';
+import SparkleIcon from '@/assets/icons/sparkle.svg';
 import { HeroOrbit } from '@/components/HeroOrbit';
 
 export const HeroSection = () => {
@@ -25,6 +26,19 @@ export const HeroSection = () => {
       </HeroOrbit>
       <HeroOrbit size={590} rotation={98}>
         <StarIcon className="size-8 text-emerald-300" />
+      </HeroOrbit>
+
+      <HeroOrbit size={430} rotation={-14}>
+        <SparkleIcon className="size-8 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={440} rotation={79}>
+        <SparkleIcon className="size-5 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={530} rotation={178}>
+        <SparkleIcon className="size-10 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={710} rotation={144}>
+        <SparkleIcon className="size-14 text-emerald-300/20" />
       </HeroOrbit>
 
       <div className="container">
