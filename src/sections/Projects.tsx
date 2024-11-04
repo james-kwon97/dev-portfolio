916c128b24c3eca9cf629236844a69e0d33fbc2b
James Kwon <jhk1097@gmail.com>
2024-11-04 21:57:32 +1300

    Finished styling project box outline

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -55,9 +55,12 @@ export const ProjectsSection = () => {
         <p className="text-center text-white/60 mt-4">
           See how I transformed designs into engaging user web experiences
         </p>
-        <div>
+        <div className="flex flex-col mt-10">
           {portfolioProjects.map((project) => (
-            <div key={project.title}>
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20"
+            >
               <div>
                 <span>{project.company}</span>
                 <span>{project.year}</span>
