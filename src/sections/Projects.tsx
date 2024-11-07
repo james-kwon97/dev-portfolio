021e26b4953a03e3f180509e1303b3315da5bee1
James Kwon <jhk1097@gmail.com>
2024-11-07 23:35:27 +1300

    Wrapping project card contents inside a new div

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -68,28 +68,30 @@ export const ProjectsSection = () => {
                 className="absolute inset-0 -z-10 opacity-5"
                 style={{ backgroundImage: `url(${grainImage.src})` }}
               ></div>
-              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                <span>{project.company}</span>
-                <span>&bull;</span>
-                <span>{project.year}</span>
+              <div className="lg:grid lg:grid-cols-2">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                  <span>{project.company}</span>
+                  <span>&bull;</span>
+                  <span>{project.year}</span>
+                </div>
+                <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
+                <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                  {project.results.map((result) => (
+                    <li className="flex gap-2 text-sm md:text-base text-white/50">
+                      <CheckCircleIcon className="size-5 md:size-6" />
+                      <span>{result.title}</span>
+                    </li>
+                  ))}
+                </ul>
+                <a href={project.link}>
+                  <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                    <span>Visit Live Site</span>
+                    <ArrowUpRightIcon className="size-4" />
+                  </button>
+                </a>
+                <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0" />
               </div>
-              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-              <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                {project.results.map((result) => (
-                  <li className="flex gap-2 text-sm md:text-base text-white/50">
-                    <CheckCircleIcon className="size-5 md:size-6" />
-                    <span>{result.title}</span>
-                  </li>
-                ))}
-              </ul>
-              <a href={project.link}>
-                <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                  <span>Visit Live Site</span>
-                  <ArrowUpRightIcon className="size-4" />
-                </button>
-              </a>
-              <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0" />
             </div>
           ))}
         </div>
