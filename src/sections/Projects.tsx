7dbf889f73802af96ff94653d6be36cdbe6f5584
James Kwon <jhk1097@gmail.com>
2024-11-08 18:05:46 +1300

    Adjusting project card's image

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -92,11 +92,11 @@ export const ProjectsSection = () => {
                     </button>
                   </a>
                 </div>
-                <div>
+                <div className="relative">
                   <Image
                     src={project.image}
                     alt={project.title}
-                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full"
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
                   />
                 </div>
               </div>
