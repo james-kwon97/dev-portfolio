f867d28101ba8d239724dc2ffdff28cd277e5542
James Kwon <jhk1097@gmail.com>
2024-11-09 21:15:58 +1300

    Centering contents to center and styling font colours

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -11,14 +11,18 @@ const words = [
 
 export const TapeSection = () => {
   return (
-    <div>
-      <div className="flex gap-4">
-        {words.map((word) => (
-          <div key={word} className="inline-flex gap-4">
-            <span>{word}</span>
-            <StarIcon className="size-6" />
+    <div className="py-16">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip">
+        <div className="flex">
+          <div className="flex flex-none gap-4">
+            {words.map((word) => (
+              <div key={word} className="inline-flex gap-4 items-center">
+                <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
+                <StarIcon className="size-6 text-gray-900" />
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
