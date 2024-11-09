7dbaa1fcd3b8e2b7dc6434b79b9cdf3da300a66f
James Kwon <jhk1097@gmail.com>
2024-11-09 21:12:18 +1300

    Adding flex properties to align each word with each star icons

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -12,12 +12,14 @@ const words = [
 export const TapeSection = () => {
   return (
     <div>
-      {words.map((word) => (
-        <div key={word}>
-          <span>{word}</span>
-          <StarIcon />
-        </div>
-      ))}
+      <div className="flex gap-4">
+        {words.map((word) => (
+          <div key={word} className="inline-flex gap-4">
+            <span>{word}</span>
+            <StarIcon className="size-6" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
