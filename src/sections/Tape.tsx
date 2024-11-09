16daadd9783c6be758e628cfc6303f8ad87cd2fa
James Kwon <jhk1097@gmail.com>
2024-11-09 21:08:20 +1300

    Adding descriptive words to a list

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,12 @@
+const words = [
+  'Performant',
+  'Accessible',
+  'Responsive',
+  'Interactive',
+  'Scalable',
+  'User Friendly',
+];
+
 export const TapeSection = () => {
   return <div>Tape Section</div>;
 };
