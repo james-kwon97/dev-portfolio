ab1ef463d95614ee9685cbb74c0bc57a9bb85091
James Kwon <jhk1097@gmail.com>
2024-11-10 21:46:03 +1300

    Creating a reusable card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import grainImage from '@/assets/images/grain.jpg';
+
+export const Card = () => {
+  return (
+    <div className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 ">
+      <div
+        className="absolute inset-0 -z-10 opacity-5"
+        style={{ backgroundImage: `url(${grainImage.src})` }}
+      ></div>
+    </div>
+  );
+};
