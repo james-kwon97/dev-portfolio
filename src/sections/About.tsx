6af34a7e8f4f0473cf931d8b784a5db86763c8fe
James Kwon <jhk1097@gmail.com>
2024-11-12 19:14:48 +1300

    Importing and using TechIcon

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -9,6 +9,7 @@ import CssIcon from '@/assets/icons/css3.svg';
 import ReactIcon from '@/assets/icons/react.svg';
 import ChromeIcon from '@/assets/icons/chrome.svg';
 import GithubIcon from '@/assets/icons/github.svg';
+import { TechIcon } from '@/components/TechIcon';
 
 const techStackItems = [
   {
