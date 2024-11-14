c2437b2fd126a47b94df819f5244f7744b299853
James Kwon <jhk1097@gmail.com>
2024-11-14 23:15:18 +1300

    Adding title and description props

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -1,13 +1,13 @@
 import StarIcon from '@/assets/icons/star.svg';
 
-export const CardHeader = () => {
+export const CardHeader = ({ title, description }: { title: string; description: string }) => {
   return (
     <div className="flex flex-col">
       <div className="inline-flex items-center gap-2">
         <StarIcon className="size-9 text-emerald-300" />
-        <h3 className="font-serif text-3xl">My Reads</h3>
+        <h3 className="font-serif text-3xl">{title}</h3>
       </div>
-      <p className="text-sm text-white/60 mt-2">Explore the books shaping my perspectives.</p>
+      <p className="text-sm text-white/60 mt-2">{description}</p>
     </div>
   );
 };
