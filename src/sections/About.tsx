a79528c52e53cbb9cd90b650a10a7fea70d02d6f
James Kwon <jhk1097@gmail.com>
2024-11-15 16:20:58 +1300

    Adding tailwind styling to my tech stack card content

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -96,9 +96,12 @@ export const AboutSection = () => {
             />
             <div>
               {techStackItems.map((item) => (
-                <div key={item.title}>
+                <div
+                  key={item.title}
+                  className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+                >
                   <TechIcon component={item.iconType} />
-                  <span>{item.title}</span>
+                  <span className="font-semibold">{item.title}</span>
                 </div>
               ))}
             </div>
