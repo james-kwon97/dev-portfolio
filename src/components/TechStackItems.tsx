07ac9645dfe6d7f4a793196ee79e20aafaecddea
James Kwon <jhk1097@gmail.com>
2024-11-15 16:41:44 +1300

    Hiding the left and right borders of the tech stack items

--- src/components/TechStackItems.tsx
+++ src/components/TechStackItems.tsx
@@ -6,16 +6,18 @@ export const TechStackItems = ({
   items: { title: string; iconType: React.ElementType }[];
 }) => {
   return (
-    <div>
-      {items.map((item) => (
-        <div
-          key={item.title}
-          className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
-        >
-          <TechIcon component={item.iconType} />
-          <span className="font-semibold">{item.title}</span>
-        </div>
-      ))}
+    <div className="flex [mask-image]:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+      <div className="flex flex-none">
+        {items.map((item) => (
+          <div
+            key={item.title}
+            className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+          >
+            <TechIcon component={item.iconType} />
+            <span className="font-semibold">{item.title}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
