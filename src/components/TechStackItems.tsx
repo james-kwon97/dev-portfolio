fa991586dab801e897e1a34a940f9f1c9ab48a96
James Kwon <jhk1097@gmail.com>
2024-11-15 16:27:36 +1300

    Creating a new TechStackItems file to import into the about cards

---
+++ src/components/TechStackItems.tsx
@@ -0,0 +1,21 @@
+import { TechIcon } from './TechIcon';
+
+export const TeckStackItems = ({
+  techStackItems,
+}: {
+  techStackItems: { title: string; iconType: React.ElementType }[];
+}) => {
+  return (
+    <div>
+      {techStackItems.map((item) => (
+        <div
+          key={item.title}
+          className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+        >
+          <TechIcon component={item.iconType} />
+          <span className="font-semibold">{item.title}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
