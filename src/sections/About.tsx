8471fbb791b654111190c3a5966aa91716843026
James Kwon <jhk1097@gmail.com>
2024-11-19 18:31:53 +1300

    First two cards are styled correctly for my tablet views

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -97,24 +97,24 @@ export const AboutSection = () => {
           description="Learn more about who I am, what I do, and what inspires me"
         />
         <div className="mt-20 flex flex-col gap-8">
-          <div className="md:grid md:grid-cols-5">
-            <Card className="h-[320px]">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5">
+            <Card className="h-[320px] md:col-span-2">
               <CardHeader
                 title="My Reads"
                 description="Explore the books shaping my perspectives."
               />
-              <div className="w-40 mx-auto mt-8">
+              <div className="w-40 mx-auto mt-2 md:mt-0">
                 <Image src={bookImage} alt="Book Cover" />
               </div>
             </Card>
 
-            <Card className="h-[320px] p-0">
+            <Card className="h-[320px] md:col-span-3">
               <CardHeader
                 title="My Tech Stack"
                 description="View my tech stack for building modern web experiences."
-                className="px-6 pt-6"
+                className=""
               />
-              <TechStackItems items={techStackItems} className="mt-6" />
+              <TechStackItems items={techStackItems} className="" />
               <TechStackItems
                 items={techStackItems}
                 className="mt-6"
