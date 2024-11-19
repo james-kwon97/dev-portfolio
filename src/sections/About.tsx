9f2ff35af9d6eeeec8bc2b784764cb88c7ca2a65
James Kwon <jhk1097@gmail.com>
2024-11-19 18:22:59 +1300

    Adding grid properties to the first two card components inside the tablet views

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -97,26 +97,31 @@ export const AboutSection = () => {
           description="Learn more about who I am, what I do, and what inspires me"
         />
         <div className="mt-20 flex flex-col gap-8">
-          <Card className="h-[320px]">
-            <CardHeader title="My Reads" description="Explore the books shaping my perspectives." />
-            <div className="w-40 mx-auto mt-8">
-              <Image src={bookImage} alt="Book Cover" />
-            </div>
-          </Card>
+          <div className="md:grid md:grid-cols-5">
+            <Card className="h-[320px]">
+              <CardHeader
+                title="My Reads"
+                description="Explore the books shaping my perspectives."
+              />
+              <div className="w-40 mx-auto mt-8">
+                <Image src={bookImage} alt="Book Cover" />
+              </div>
+            </Card>
 
-          <Card className="h-[320px] p-0">
-            <CardHeader
-              title="My Tech Stack"
-              description="View my tech stack for building modern web experiences."
-              className="px-6 pt-6"
-            />
-            <TechStackItems items={techStackItems} className="mt-6" />
-            <TechStackItems
-              items={techStackItems}
-              className="mt-6"
-              itemsWrapperClassName="-translate-x-1/2"
-            />
-          </Card>
+            <Card className="h-[320px] p-0">
+              <CardHeader
+                title="My Tech Stack"
+                description="View my tech stack for building modern web experiences."
+                className="px-6 pt-6"
+              />
+              <TechStackItems items={techStackItems} className="mt-6" />
+              <TechStackItems
+                items={techStackItems}
+                className="mt-6"
+                itemsWrapperClassName="-translate-x-1/2"
+              />
+            </Card>
+          </div>
 
           <Card className="h-[320px] p-0 flex flex-col">
             <CardHeader
