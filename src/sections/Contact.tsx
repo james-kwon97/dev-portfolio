9e18d0a985f6a1216445fc8ae93fa7d44abe4d70
James Kwon <jhk1097@gmail.com>
2024-11-21 03:33:45 +1300

    Adding grainImage background on top of our contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,10 +1,15 @@
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
+import grainImage from '@/assets/images/grain.jpg';
 
 export const ContactSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 pt-14">
       <div className="container">
-        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-5 -z-10"
+            style={{ backgroundImage: `url(${grainImage.src})` }}
+          ></div>
           <h2 className="font-serif text-2xl">Let's create something amazing together</h2>
           <p className="text-sm mt-2">
             Ready to bring your next project to life? Let's connect and discuss how I can help you
