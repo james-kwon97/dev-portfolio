a141e0e2510844f3d8c203efd257a3e7bdc7cd28
James Kwon <jhk1097@gmail.com>
2024-11-24 21:54:19 +1300

    Adding spinDuration and orbitDurations

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -7,11 +7,15 @@ export const HeroOrbit = ({
   rotation,
   shouldOrbit = false,
   orbitDuration,
+  shouldSpin = false,
+  spinDuration,
 }: PropsWithChildren<{
   size: number;
   rotation: number;
   shouldOrbit?: boolean;
+  shouldSpin?: boolean;
   orbitDuration?: string;
+  spinDuration?: string;
 }>) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -23,8 +27,13 @@ export const HeroOrbit = ({
           className="flex items-start justify-start"
           style={{ transform: `rotate(${rotation}deg)`, height: `${size}px`, width: `${size}px` }}
         >
-          <div className="inline-flex" style={{ transform: `rotate(${rotation * -1}deg)` }}>
-            {children}
+          <div
+            className={twMerge(shouldSpin === true && 'animate-spin')}
+            style={{ animationDuration: spinDuration }}
+          >
+            <div className="inline-flex" style={{ transform: `rotate(${rotation * -1}deg)` }}>
+              {children}
+            </div>
           </div>
         </div>
       </div>
