3f0b3eebc3325cb49d83d526c7fe263baf0cabd1
James Kwon <jhk1097@gmail.com>
2024-11-26 17:08:18 +1300

    Sticking our projects near the top for a smooth animation

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -63,7 +63,7 @@ export const ProjectsSection = () => {
           {portfolioProjects.map((project) => (
             <Card
               key={project.title}
-              className=" px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 "
+              className=" px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky top-16"
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
