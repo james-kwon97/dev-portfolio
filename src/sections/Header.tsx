fd3c1e74bc604a07b13eb86784b54aff0cdeca58
James Kwon <jhk1097@gmail.com>
2024-11-28 14:45:38 +1300

    Home nav item highlighted in white as default

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,7 +2,10 @@ export const Header = () => {
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a href="#home" className="nav-item ">
+        <a
+          href="#home"
+          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+        >
           Home
         </a>
         <a href="#projects" className="nav-item">
@@ -11,10 +14,7 @@ export const Header = () => {
         <a href="#about" className="nav-item">
           About
         </a>
-        <a
-          href="#contact"
-          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
-        >
+        <a href="#contact" className="nav-item">
           Contact
         </a>
       </nav>
