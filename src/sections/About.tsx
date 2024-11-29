bc4999c9ec3a464283a674f1249b968224feddfc
James Kwon <jhk1097@gmail.com>
2024-11-29 16:15:12 +1300

    About section is completed

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,6 +1,5 @@
 import { SectionHeader } from '@/components/SectionHeader';
 import { Card } from '@/components/Card';
-import StarIcon from '@/assets/icons/star.svg';
 import bookImage from '@/assets/images/book-cover.png';
 import Image from 'next/image';
 import JavascriptIcon from '@/assets/icons/square-js.svg';
@@ -9,7 +8,6 @@ import CssIcon from '@/assets/icons/css3.svg';
 import ReactIcon from '@/assets/icons/react.svg';
 import ChromeIcon from '@/assets/icons/chrome.svg';
 import GithubIcon from '@/assets/icons/github.svg';
-import { TechIcon } from '@/components/TechIcon';
 import mapImage from '@/assets/images/map.png';
 import smileMemoji from '@/assets/images/memoji-smile.png';
 import { CardHeader } from '@/components/CardHeader';
