08a1f96b612024d816324072da10e2e86b20e47f
James Kwon <jhk1097@gmail.com>
2024-11-30 14:47:00 +1300

    Using client and adding active state management

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,4 +1,10 @@
+'use client';
+
+import { useState } from 'react';
+
 export const Header = () => {
+  const [active, setActive] = useState('home');
+
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
